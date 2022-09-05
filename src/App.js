ee3bd45465e828442108b7c1f3b13552da6dc39f
stepanetsaleksandr <stepanetsaleksandr@gmail.com>
2022-09-05 13:38:34 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,196 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <header>Розклад</header>
+      <main>
+        <div className='container'>
+          <div className='block'>
+            <div className='day shadow p-3 mb-5 bg-body rounded border border-success border-opacity-25'>
+              <h2>Понеділок</h2>
+              <div className='day-body w-100 '>
+                <ul class='list-group'>
+                  <li class='list-group-item d-flex justify-content-between align-items-start'>
+                    <a
+                      class='nav-link active text-primary'
+                      aria-current='page'
+                      href='https://us04web.zoom.us/j/2308136977?pwd=51ToKa4JoxxJfy8JZy0vVc7f4YJ3Yq.1'
+                      target='_blank'
+                      rel='noreferrer'
+                    >
+                      Я досліджую світ
+                    </a>
+                    <div>8:30</div>
+                  </li>
+                  <li class='list-group-item d-flex justify-content-between align-items-start'>
+                    <a
+                      class='nav-link active text-primary'
+                      aria-current='page'
+                      href='https://us04web.zoom.us/j/8516787587?pwd=bVFRaWplQzNuVGNpcjZkQXVsQVdUdz09'
+                      target='_blank'
+                      rel='noreferrer'
+                    >
+                      Інформатика
+                    </a>
+                    <div>9:25</div>
+                  </li>
+                  <li class='list-group-item d-flex justify-content-between align-items-start'>
+                    <a
+                      class='nav-link active text-primary'
+                      aria-current='page'
+                      href='https://us04web.zoom.us/j/2308136977?pwd=51ToKa4JoxxJfy8JZy0vVc7f4YJ3Yq.1'
+                      target='_blank'
+                      rel='noreferrer'
+                    >
+                      Математика
+                    </a>
+                    <div>10:30</div>
+                  </li>
+                  <li class='list-group-item d-flex justify-content-between align-items-start'>
+                    <a
+                      class='nav-link active text-primary'
+                      aria-current='page'
+                      href='https://us04web.zoom.us/j/2308136977?pwd=51ToKa4JoxxJfy8JZy0vVc7f4YJ3Yq.1'
+                      target='_blank'
+                      rel='noreferrer'
+                    >
+                      Читання
+                    </a>
+                    <div>11:35</div>
+                  </li>
+                  <li class='list-group-item d-flex justify-content-between align-items-start'>
+                    <a
+                      class='nav-link active text-primary'
+                      aria-current='page'
+                      href='https://us04web.zoom.us/j/2308136977?pwd=51ToKa4JoxxJfy8JZy0vVc7f4YJ3Yq.1'
+                      target='_blank'
+                      rel='noreferrer'
+                    >
+                      Англійська мова
+                    </a>
+                    <div>8:30</div>
+                  </li>
+                </ul>
+              </div>
+            </div>
+
+            <div className='day shadow p-3 mb-5 bg-body rounded border border-success border-opacity-25'>
+              <h2>Вівторок</h2>
+              <div className='day-body w-100'>
+                <ul class='list-group'>
+                  <li class='list-group-item d-flex justify-content-between align-items-start'>
+                    <div>1. Урок</div>
+                    <div>10:30</div>
+                  </li>
+                  <li class='list-group-item d-flex justify-content-between align-items-start'>
+                    <div>1. Урок</div>
+                    <div>10:30</div>
+                  </li>
+                  <li class='list-group-item d-flex justify-content-between align-items-start'>
+                    <div>1. Урок</div>
+                    <div>10:30</div>
+                  </li>
+                  <li class='list-group-item d-flex justify-content-between align-items-start'>
+                    <div>1. Урок</div>
+                    <div>10:30</div>
+                  </li>
+                  <li class='list-group-item d-flex justify-content-between align-items-start'>
+                    <div>1. Урок</div>
+                    <div>10:30</div>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div className='block'>
+            <div className='day shadow p-3 mb-5 bg-body rounded border border-success border-opacity-25'>
+              <h2>Середа</h2>
+              <div className='day-body w-100'>
+                <ul class='list-group'>
+                  <li class='list-group-item d-flex justify-content-between align-items-start'>
+                    <div>1. Урок</div>
+                    <div>10:30</div>
+                  </li>
+                  <li class='list-group-item d-flex justify-content-between align-items-start'>
+                    <div>1. Урок</div>
+                    <div>10:30</div>
+                  </li>
+                  <li class='list-group-item d-flex justify-content-between align-items-start'>
+                    <div>1. Урок</div>
+                    <div>10:30</div>
+                  </li>
+                  <li class='list-group-item d-flex justify-content-between align-items-start'>
+                    <div>1. Урок</div>
+                    <div>10:30</div>
+                  </li>
+                  <li class='list-group-item d-flex justify-content-between align-items-start'>
+                    <div>1. Урок</div>
+                    <div>10:30</div>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className='day shadow p-3 mb-5 bg-body rounded border border-success border-opacity-25'>
+              <h2>Четвер</h2>
+              <div className='day-body w-100'>
+                <ul class='list-group'>
+                  <li class='list-group-item d-flex justify-content-between align-items-start'>
+                    <div>1. Урок</div>
+                    <div>10:30</div>
+                  </li>
+                  <li class='list-group-item d-flex justify-content-between align-items-start'>
+                    <div>1. Урок</div>
+                    <div>10:30</div>
+                  </li>
+                  <li class='list-group-item d-flex justify-content-between align-items-start'>
+                    <div>1. Урок</div>
+                    <div>10:30</div>
+                  </li>
+                  <li class='list-group-item d-flex justify-content-between align-items-start'>
+                    <div>1. Урок</div>
+                    <div>10:30</div>
+                  </li>
+                  <li class='list-group-item d-flex justify-content-between align-items-start'>
+                    <div>1. Урок</div>
+                    <div>10:30</div>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div className='block'>
+            <div className='day shadow p-3 mb-5 bg-body rounded border border-success border-opacity-25'>
+              <h2>П'ятниця</h2>
+              <div className='day-body w-100'>
+                <ul class='list-group'>
+                  <li class='list-group-item d-flex justify-content-between align-items-start'>
+                    <div>1. Урок</div>
+                    <div>10:30</div>
+                  </li>
+                  <li class='list-group-item d-flex justify-content-between align-items-start'>
+                    <div>1. Урок</div>
+                    <div>10:30</div>
+                  </li>
+                  <li class='list-group-item d-flex justify-content-between align-items-start'>
+                    <div>1. Урок</div>
+                    <div>10:30</div>
+                  </li>
+                  <li class='list-group-item d-flex justify-content-between align-items-start'>
+                    <div>1. Урок</div>
+                    <div>10:30</div>
+                  </li>
+                  <li class='list-group-item d-flex justify-content-between align-items-start'>
+                    <div>1. Урок</div>
+                    <div>10:30</div>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+      <footer></footer>
     </div>
   );
 }
